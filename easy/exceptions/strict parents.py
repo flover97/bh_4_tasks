18486149c1a2e17e406898d42e9ab53a75fd33db
flover <flo.fd@yandex.ru>
2021-02-26 20:55:06 +0300

    done

--- easy/exceptions/strict parents.py
+++ easy/exceptions/strict parents.py
@@ -55,3 +55,18 @@ def random_gift():
 if __name__ == '__main__':
     print("Просим показать ребенка дневник\n")
     gift = None
+    try:
+        score = get_score()
+        if score < 7:
+            raise ValueError(score)
+        print('Ребенок получил хорошую оценку')
+        gift = random_gift()
+    except RuntimeError as exc:
+        print(exc)
+        punish_dog()
+    except ValueError as exc:
+        punish_child(exc)
+    else:
+        praise(gift)
+    finally:
+        cook_dinner()
