cfb1728aab0af1a999781fdd9285cc341a3f4378
flover <flo.fd@yandex.ru>
2021-03-02 18:38:59 +0300

    done

--- easy/cycles/odd.py
+++ easy/cycles/odd.py
@@ -18,7 +18,11 @@ odd_in_list([2, -1, 4, True, None, 6]) -> [2, 4, 6]
 
 def odd_in_list(some_list: list) -> list:
     odd_list = []
-    # TODO тут написать цикл for
+    for item in some_list:
+        if not isinstance(item, int) or isinstance(item, bool):
+            continue
+        if item % 2 == 0:
+            odd_list.append(item)
     return odd_list
 
 
